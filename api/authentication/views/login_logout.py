6c929c9f08c1014141ec43b85d1a7a3d2216e526
Matt McCarthy <matt@mccarthycode.com>
2021-04-09 19:25:42 -0500

    Separate login/out views from registration views

---
+++ api/authentication/views/login_logout.py
@@ -0,0 +1,73 @@
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from ..serializers import *
+
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
+
+
+class LoginView(GenericAPIView):
+    """
+    View for taking in an existing user's credentials and authorizing them if valid or denying access if invalid.
+    """
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        """
+        POST method for taking a token from a query string, checking if it is valid, and logging in the user if valid, or returning an error response if invalid.
+        """
+        serializer = self.serializer_class(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            return Response(
+                {
+                    'errors': ['The credentials used to login were invalid.'],
+                    **e.detail,
+                },
+                status=status.HTTP_400_BAD_REQUEST)
+        except AuthenticationFailed as e:
+            return Response(
+                {'errors': [str(e)]}, status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class LogoutView(GenericAPIView):
+    """
+    View for taking in a user's access token and logging them out.
+    """
+    serializer_class = LogoutSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        """
+        POST method for taking a token from a request body, checking if it is valid, and logging out the user if valid, or returning an error response if invalid.
+        """
+        auth = request.META.get('HTTP_AUTHORIZATION', None).split(' ')
+
+        try:
+            assert auth[0] == 'Bearer'
+        except AssertionError:
+            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+
+        serializer = self.serializer_class(data={'access': auth[1]})
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            return Response(
+                {
+                    'errors': [
+                        'The token used to logout was invalid. You may have successfully logged out already.'
+                    ],
+                    **e.detail,
+                },
+                status=status.HTTP_400_BAD_REQUEST)
+
+        body = serializer.save()
+
+        return Response(body, status=status.HTTP_205_RESET_CONTENT)
--- api/authentication/views/registration.py
+++ api/authentication/views/registration.py
@@ -1,22 +1,19 @@
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
-from django.db.utils import IntegrityError
-from django.shortcuts import get_object_or_404
 from django.urls import reverse
 
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .models import User
-from .serializers import *
-from .utils import Util
+from ..models import User
+from ..serializers import *
+from ..utils import Util
 
 from jwt.exceptions import DecodeError, ExpiredSignatureError
-from rest_framework.exceptions import AuthenticationFailed, ValidationError
+from rest_framework.exceptions import ValidationError
 from api.authentication.exceptions import ConflictError
 
 
@@ -107,68 +104,3 @@ class RegisterVerifyView(APIView):
             },
             status=status.HTTP_200_OK)
 
-
-class LoginView(GenericAPIView):
-    """
-    View for taking in an existing user's credentials and authorizing them if valid or denying access if invalid.
-    """
-    serializer_class = LoginSerializer
-
-    def post(self, request):
-        """
-        POST method for taking a token from a query string, checking if it is valid, and logging in the user if valid, or returning an error response if invalid.
-        """
-        serializer = self.serializer_class(data=request.data)
-
-        try:
-            serializer.is_valid(raise_exception=True)
-        except ValidationError as e:
-            return Response(
-                {
-                    'errors': ['The credentials used to login were invalid.'],
-                    **e.detail,
-                },
-                status=status.HTTP_400_BAD_REQUEST)
-        except AuthenticationFailed as e:
-            return Response(
-                {'errors': [str(e)]}, status=status.HTTP_401_UNAUTHORIZED)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class LogoutView(GenericAPIView):
-    """
-    View for taking in a user's access token and logging them out.
-    """
-    serializer_class = LogoutSerializer
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        """
-        POST method for taking a token from a request body, checking if it is valid, and logging out the user if valid, or returning an error response if invalid.
-        """
-        auth = request.META.get('HTTP_AUTHORIZATION', None).split(' ')
-
-        try:
-            assert auth[0] == 'Bearer'
-        except AssertionError:
-            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
-
-        serializer = self.serializer_class(
-            data={'access': auth[1]})
-
-        try:
-            serializer.is_valid(raise_exception=True)
-        except ValidationError as e:
-            return Response(
-                {
-                    'errors': [
-                        'The token used to logout was invalid. You may have successfully logged out already.'
-                    ],
-                    **e.detail,
-                },
-                status=status.HTTP_400_BAD_REQUEST)
-
-        body = serializer.save()
-
-        return Response(body, status=status.HTTP_205_RESET_CONTENT)
